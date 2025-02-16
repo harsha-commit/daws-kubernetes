f9d1bb1341872ee0c3a1d82ce25b94a8c4ae16a6
Harsha Vardhan Bashavathini <harshavardhan.bashavathini@gmail.com>
2025-02-16 20:17:16 +0530

    Update 060

--- 060/solution2/k8s.sh
+++ 060/solution2/k8s.sh
@@ -28,5 +28,4 @@ chmod +x ./kubectl
 mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$HOME/bin:$PATH
 echo 'export PATH=$HOME/bin:$PATH' >> ~/.bashrc
 
-# Installing EBS Drivers
-kubectl apply -k "github.com/kubernetes-sigs/aws-ebs-csi-driver/deploy/kubernetes/overlays/stable/?ref=release-1.39"
+# Installing Helm
